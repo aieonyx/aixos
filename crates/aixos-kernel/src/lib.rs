20b83020b1c005f511e9288668fd3b9e3af00649
Ed <aieonyx.eu@gmail.com>
2026-07-29 12:47:54 +0200

    fix(CI): add unreachable_code allows to aixos-kernel lib.rs
    Copyright (c) 2026 Edison Lepiten / AIEONYX

--- crates/aixos-kernel/src/lib.rs
+++ crates/aixos-kernel/src/lib.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unreachable_code)]
 // Copyright (c) 2026 Edison Lepiten / AIEONYX
 // SPDX-License-Identifier: Apache-2.0
 #![cfg_attr(not(test), no_std)]
